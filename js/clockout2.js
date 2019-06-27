0b5deb7bb9a46ca7d0b23613a983d4f4b1e9fbf8
Dafydd <dafyddzephyr@gmail.com>
2019-06-27 08:46:24 -0700

    minor bug fixes

--- js/clockout2.js
+++ js/clockout2.js
@@ -7,23 +7,22 @@ $(() => {
 
     calculate = () => {
         let wh = Number($('#wh').val());
-        let wm = Number($('#wm').val()) - 7;
+        let wm = Number($('#wm').val());
         let ch = Number($('#ch').val()) + wh;
-        let cm = Math.round(Number($('#cm').val()) / 15) * 15;
+        let cm = Math.round(Number($('#cm').val()) / 15) * 15 + wm - 60;
         console.log(`work minutes ${cm}`);
         if (wh >= 6 && wm > 0 || wh > 6) {
             wm += 30;
         }
-        for (let i = 0; i < wm; i++) {
-            cm++;
-            if (cm === 60) {
-                ch += 1;
-                cm = 0;
-            }
-        }
-        if (cm === 0) {
+        if (cm === 0 || cm < 10) {
             cm = 53;
             ch -= 1;
+        }
+        else if (cm >= 60) {
+            ch += 1;
+        }
+        else {
+            cm -= 7;
         }
         if (ch > 12) {
             ch = ch - 12;
@@ -31,6 +30,7 @@ $(() => {
         let time;
         cm < 10 ? time = ch + ':0' + cm : time = ch + ':' + cm;
         display.text(time);
+        
     }
 
     button.click(() => {
